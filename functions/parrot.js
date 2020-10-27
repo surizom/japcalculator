eb7d30418082796ba62014d446b60630eb266015
Moaad Fattali <f.moaad@gmail.com>
2020-10-27 12:01:18 +0100

    request message on empty one

--- functions/parrot.js
+++ functions/parrot.js
@@ -1,12 +1,18 @@
 exports.handler = async function (event, context) {
 
-    const requestBody = JSON.parse(event.body);
-    const message = requestBody.message;
+    const requestBody;
+    try {
+        requestBody = JSON.parse(event.body);
+    } catch (exception) {
+        requestBody = null;
+    }
+
+    const message = requestBody ? requestBody.message : null;
 
     if (!message) {
         return {
             statusCode: 400,
-            body: "Message po trouvé",
+            body: "Envoie un message frr",
         };
     }
 
